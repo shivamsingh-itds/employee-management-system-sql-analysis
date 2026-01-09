1027748093028febd0d8a91edad7f6f5ca923d87
shivamsingh-itds <shivam.itds@gmail.com>
2026-01-09 22:39:07 +0530

    Department Record

---
+++ department.sql
@@ -0,0 +1,13 @@
+INSERT INTO departments (department_name, location, budget) VALUES
+('IT Services', '1 floor', 50000000.00),
+('Consulting', '3 floor', 75000000.00),
+('Human Resources', '6 floor', 15000000.00),
+('Finance', '1 floor', 20000000.00),
+('Marketing', '4 floor', 18000000.00),
+('Data & AI', '6 floor', 60000000.00);
+
+select * from departments;
+
+drop TABLE departments;
+
+drop DATABASE employee_management;
